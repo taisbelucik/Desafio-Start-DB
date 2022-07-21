e4341f0764de6d3d25020aeb240110078db85318
taisbelucik <taisbelucik@hotmail.com>
2022-07-21 13:18:53 -0300

    verificar letra digitada

--- desafio/desafio.js
+++ desafio/desafio.js
@@ -1,11 +1,18 @@
-const readline = require('readline-sync');
-const Forca = require('./forca');
+//desafio = utiliza a classe forca pra utilizar os métodos dela
+//pega o que o usuário digita
 
-const jogo = new Forca('abacaxi');
 
-while (!["perdeu", "ganhou"].includes(jogo.buscarEstado())) {
-    const chute = readline.question("Aguardando chute: \n");
-    jogo.chutar(chute);
+const readline = require('readline-sync'); // chama o arquivo readline-sync que permite que sejam informados dados ao sistema utilizando-se da linha de comando.
+const Forca = require('./forca'); // chama o arquivo forca.js
+
+
+const jogo = new Forca('abacaxi'); // gera um objeto a partir da classe Forca cria um objeto para ter acesso aos métodos da classe Forca
+
+
+
+while (!["perdeu", "ganhou"].includes(jogo.buscarEstado())) { //Enquando não perder e não ganhar quero que o jogo continue e faça...
+    const chute = readline.question("Aguardando chute: \n"); // Fazer no terminal
+    jogo.chutar(chute);  //adiciona em jogo o metodo chutar passando chute
     console.log(jogo.buscarDadosDoJogo());
 }
 
--- desafio/forca.js
+++ desafio/forca.js
@@ -1,15 +1,55 @@
+// Foca é a classe que possui os métodos para o jogo funcionar
+// possui os métodos chutar() buscaEstado() e buscarDadosDoJogo()
+
 class Forca {
+   palavra = ''
+  constructor(palavraSecreta){
+    this.palavra = palavraSecreta
 
-  chutar(letra) { }
+  }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
-  buscarDadosDoJogo() {
-      return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      }
+  chutar(letra) {
+    if (letra.length > 1) {
+      console.log("Não é permitido digitar mais de uma palavra por jogada");
+    }else{
+    const acertoLetra = this.verificarLetra(letra, this.palavra )
+     if(acertoLetra === true){
+      console.log('Foi encontrado uma letra na palavra')
+     } else {
+      //console.log('Letra incorreta')
+      // console.log("chegou aqui", this.buscarDadosDoJogo)
+      this.buscarDadosDoJogo(null, true, null)
+     }
+
+    }
+  }
+
+  verificarLetra(letraDigitada, palavraSecreta){
+   return palavraSecreta.includes(letraDigitada)
+  }
+
+  buscarEstado(status) {
+    return status;
+  } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+
+  buscarDadosDoJogo(pLetrasChutadas, pVidas, pPalavra) {
+    const letrasChutadas = [] // Deve conter todas as letras chutadas
+    let vidas = 6 // Quantidade de vidas restantes
+    const palavra = []
+    debugger
+    if(pVidas === true) {
+      console.log("chegou aqui")
+      vidas = --vidas
+      console.log(vidas)
+    } 
+
+    const dadosJogo = {
+      letrasChutadas, // Deve conter todas as letras chutadas
+      vidas, // Quantidade de vidas restantes
+      palavra, // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+    }
+    return dadosJogo
   }
 }
 
