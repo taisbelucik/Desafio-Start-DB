a96d36d0e1240d21842e4b4ed5a75b7acb52ce55
taisbelucik <taisbelucik@hotmail.com>
2022-07-24 20:14:30 -0300

    status do jogo

--- desafio/forca.js
+++ desafio/forca.js
@@ -2,7 +2,7 @@
 // possui os métodos chutar() buscaEstado() e buscarDadosDoJogo()
 
 class Forca {
-  palavra = "";
+  palavra = '';
   constructor(palavraSecreta) {
     this.palavra = palavraSecreta;
     this.setArrayPalavraSecretaVazio(palavraSecreta);
@@ -14,9 +14,10 @@ class Forca {
     palavra: [], // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
   };
 
+  // PEGA O COMPRIMENTO DA PALAVRA SECRETA, CRIANDO ESPAÇOS PARA INCLUIR AS LETRAS NO ARRAY DA PALAVRA SECRETA
   setArrayPalavraSecretaVazio(palavraSecreta) {
     for (let index = 0; index < palavraSecreta.length; index++) {
-      this.dadosJogo.palavra.push("");
+      this.dadosJogo.palavra.push('');
     }
   }
 
@@ -24,6 +25,7 @@ class Forca {
     return this.dadosJogo;
   }
 
+  // TRANSFORMA EM ATRIBUTOS GLOBAIS
   setDadosDoJogo(vidas, letrasChutadas, palavra) {
     (this.dadosJogo.vidas = vidas),
       (this.dadosJogo.letrasChutadas = letrasChutadas),
@@ -32,24 +34,31 @@ class Forca {
 
   //VERIFICA SE A LETRA CHUTADA ESTÁ NA PALAVRA SECRETA
   chutar(letra) {
-    this.dadosJogo.letrasChutadas.push(letra); //inclui a letra chutada na array letrasChutadas
-    if (letra.length > 1) {
-      console.log("Não é permitido digitar mais de uma palavra por jogada");
+    if (letra.length > 1) { //não aceitar que quantidade de letras digitadas for mais que 1
+      console.log('Não é permitido digitar mais de uma palavra por jogada');
     } else {
       const acertoLetra = this.verificarLetra(letra, this.palavra);
       if (acertoLetra === true) {
-        this.verificaPosicao(letra); // chama o método que inclui a letra na posição da palavra
-        console.log("Foi encontrado uma letra na palavra!");
+        this.verificaPosicao(letra); // chama o método que inclui a letra correta na posição da palavra
+        console.log('Foi encontrado uma letra na palavra!');
       } else {
-        //se a letra estiver incorreta diminua uma vida
-        var descontoVida = this.verificaLetraChutadaErradaNovamente();
-        if (descontoVida === true) {
+        if (this.dadosJogo.letrasChutadas.length > 0) {
+          //se a letra estiver incorreta diminua uma vida
+          var descontoVida = this.verificaLetraChutadaErradaNovamente(letra);
+          if (descontoVida !== true) {
+            var descontoVida = this.dadosJogo.vidas - 1;
+            this.dadosJogo.vidas = descontoVida;
+            this.dadosJogo.letrasChutadas.push(letra); //inclui a letra chutada na array letrasChutadas
+
+            console.log('Letra incorreta!');
+          } else {
+            console.log('Você já chutou essa letra!');
+          }
+        } else {
           var descontoVida = this.dadosJogo.vidas - 1;
           this.dadosJogo.vidas = descontoVida;
-        
-          console.log("Letra incorreta!");
-        } else {
-          console.log("Você já chutou essa letra!")
+          this.dadosJogo.letrasChutadas.push(letra);
+          console.log('Letra incorreta!');
         }
       }
     }
@@ -60,15 +69,17 @@ class Forca {
     return palavraSecreta.includes(letraDigitada);
   }
 
+  //VERIFICA SE UMA LETRA QUE NÃO ESTÁ NA PALAVRA FOI DIGITADA DUAS VEZES
   verificaLetraChutadaErradaNovamente(letra) {
+    let existeLetra;
     for (let index = 0; index < this.dadosJogo.letrasChutadas.length; index++) {
       const element = this.dadosJogo.letrasChutadas[index];
       if (letra === element) {
-        return false;
-      } else {
-        return true;
+        existeLetra = true;
       }
     }
+
+    return existeLetra;
   }
 
   //COLOCA A LETRA DIGITADA NA POSIÇÃO CORRETA
@@ -98,25 +109,27 @@ class Forca {
     return palavraArray;
   }
 
+  //VERIFICA SE ACERTOU TODAS AS LETRAS ENQUANTO HAVIAM VIDAS: GANHOU
+  // SE TERMINAR AS VIDAS: PERDEU
   buscarEstado(status) {
     const statusJogo = this.verificaPalavra();
 
     if (statusJogo === true && this.dadosJogo.vidas > 0) {
-      return "ganhou";
+      return 'ganhou';
     }
 
     if (this.dadosJogo.vidas === 0) {
-      return "perdeu";
-      
+      return 'perdeu';
     }
 
     //return status; // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
   }
 
+  //VERIFICA O TAMANHO DA PALAVRA PARA INSERIR AS STRINGS VAZIAS
   verificaPalavra() {
     let contadorPalavra = 0;
     this.dadosJogo.palavra.forEach((item) => {
-      if (item !== "") {
+      if (item !== '') {
         contadorPalavra++;
       }
     });
